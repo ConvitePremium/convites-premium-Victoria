e45c44163f5f7ed2a3ee2a43a89ccef28dfa8470
ConvitePremium <essenciamusicalofc@gmail.com>
2026-09-19 04:14:33 -0300

    Update config.js

--- config.js
+++ config.js
@@ -89,9 +89,9 @@ window.CONFIG = {
   // mostrarTexto: true mostra “Voltar”; false deixa apenas a área clicável.
   // A posição pode ser alterada no editor ?editor=1.
   botoesVoltar: {
-    presentes: { mostrarTexto:false, posicao: {"left":24.2,"top":83.6,"width":55.0,"height":7.8} },
-    dresscode: { mostrarTexto:true, posicao: {"left":26.805437549920125,"top":86.88550214891683,"width":46.581485248602235,"height":5.424627114446271} },
-    manual: { mostrarTexto:true, posicao: {"left":25.783057732627793,"top":85.2744510457582,"width":47.80830358925719,"height":5.309555171188598} }
+    presentes: { mostrarTexto:false, posicao: {"left":29.10735136281949,"top":80.83820312573391,"width":41.30032572883387,"height":5.153276166430366} },
+    dresscode: { mostrarTexto:false, posicao: {"left":27.214392596845045,"top":91.58342992989473,"width":46.581485248602235,"height":5.424627114446271} },
+    manual: { mostrarTexto:false, posicao: {"left":28.64568066094249,"top":81.3619062055071,"width":42.69648250299521,"height":4.734181188196089} },
   },
 
   // ----- POSIÇÃO DOS BOTÕES NA TELA PRINCIPAL ----------------------------
@@ -100,11 +100,11 @@ window.CONFIG = {
   //   left/top: canto superior esquerdo do botão
   //   width/height: tamanho do botão
     hotspots: {
-        confirm: { left:19.111031037141306, top:63.8032059022128, width:15.963035699920129, height:8.57238650880756 },
-        map: { left:42.3313764835857, top:64.03332543810177, width:15.611963461541478, height:8.522989476077747 },
-        gift: { left:64.6203873670928, top:63.907444274051855, width:16.091044953075077, height:8.643484137471408 },
-        dress: { left:30.670926517571885, top:77.02597723193821, width:15.682105506190094, height:8.298251846116688 },
-        manual: { left:53.98780574768387, top:76.56567628964217, width:16.29551467651757, height:8.528402317264716 }
+        confirm: { left:19.92892553095121, top:59.20021403827273, width:15.963035699920129, height:8.57238650880756 },
+        map: { left:42.3313764835857, top:59.08510128280698, width:15.611963461541478, height:8.522989476077747 },
+        gift: { left:64.00697819676533, top:58.959228898267305, width:16.091044953075077, height:8.643484137471408 },
+        dress: { left:29.64856230031949, top:72.3079123273017, width:15.682105506190094, height:8.298251846116688 },
+        manual: { left:55.2146552884187, top:72.53805840869461, width:16.29551467651757, height:8.528402317264716 }
   },
 
   // ----- CONTAGEM REGRESSIVA ---------------------------------------------
